682c178c52daa5902e6e9c58bda01900e1a198e1
Ben Covey <benvcovey@gmail.com>
2020-10-24 17:47:27 -0300

    Initial Upload
    Initial Upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Auth0.ManagementApi.Models;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+
+namespace WifiNotifcation
+{
+    class Program
+    {
+
+        static string[] Scopes = { GmailService.Scope.GmailCompose };
+        static string ApplicationName = "Gmail API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            MimeMessage email = new MimeMessage();
+        }
+
+        class Email
+    {
+        bool SendEmail()
+        {
+            bool bRet = true;
+            
+
+
+
+                return bRet;
+        }
+
+     
+
+} 
+}
